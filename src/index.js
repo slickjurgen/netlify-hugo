55356e55fc015ac9280a773fd09baeecb0482792
Slick Jurgen <slickjurgen@googlemail.com>
2020-12-30 21:31:27 +0100

    Button added

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ import "./css/main.css";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
-const options = {
-    publicKey: {
-        rp: { name: "example.com" },
-        user: {
-            name: "john.appleseed@example.com",
-            id: userIdBuffer,
-            displayName: "John Appleseed"
-        },
-        pubKeyCredParams: [ { type: "public-key", alg: -7 } ],
-        challenge: challengeBuffer,
-        authenticatorSelection: { authenticatorAttachment: "platform" }
-    }
-};
+// Fetching the challengeBuffer before the onclick event.
 
-const publicKeyCredential = await navigator.credentials.create(options);
+button.addEventListener("click", async () => {
+    const options = {
+        publicKey: {
+            rp: { name: "example.com" },
+            user: {
+                name: "john.appleseed@example.com",
+                id: userIdBuffer,
+                displayName: "John Appleseed"
+            },
+            pubKeyCredParams: [ { type: "public-key", alg: -7 } ],
+            challenge: challengeBuffer,
+            authenticatorSelection: { authenticatorAttachment: "platform" }
+        }
+    };
+
+    const publicKeyCredential = await navigator.credentials.create(options);
+});
