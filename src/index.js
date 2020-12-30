bc8c237a4761c1e7ed902b951356fe633d6794d0
Slick Jurgen <slickjurgen@googlemail.com>
2020-12-30 21:19:56 +0100

    Added some code for WebAuth

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+const options = {
+    publicKey: {
+        rp: { name: "example.com" },
+        user: {
+            name: "john.appleseed@example.com",
+            id: userIdBuffer,
+            displayName: "John Appleseed"
+        },
+        pubKeyCredParams: [ { type: "public-key", alg: -7 } ],
+        challenge: challengeBuffer,
+        authenticatorSelection: { authenticatorAttachment: "platform" }
+    }
+};
+
+const publicKeyCredential = await navigator.credentials.create(options);
