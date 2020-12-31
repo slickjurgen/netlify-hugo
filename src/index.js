9f1c7db70d1787e0f8137d6af49b6c86e307f15b
Slick Jurgen <slickjurgen@googlemail.com>
2020-12-31 16:02:54 +0100

    Added some debug info

--- src/index.js
+++ src/index.js
@@ -5,22 +5,37 @@ import "./css/main.css";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
-// Fetching the challengeBuffer before the onclick event.
+var button = document.getElementById("button");
 
+// Fetching the challengeBuffer before the onclick event.
 button.addEventListener("click", async () => {
+
+    const isAvailable = await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable();
+
+    if (isAvailable) {
+        console.log("Available");
+        alert("Yeah!");
+    } else {
+        console.log("Not available");
+        alert("Uuh... no...");
+    }
+      
     const options = {
         publicKey: {
             rp: { name: "example.com" },
             user: {
                 name: "john.appleseed@example.com",
-                id: userIdBuffer,
+                id: "USER_ID_12345678910",
                 displayName: "John Appleseed"
             },
             pubKeyCredParams: [ { type: "public-key", alg: -7 } ],
-            challenge: challengeBuffer,
+//           challenge: challengeBuffer,
+            challenge: "challengeBuffer",
             authenticatorSelection: { authenticatorAttachment: "platform" }
         }
     };
 
+    console.log("Create...");
     const publicKeyCredential = await navigator.credentials.create(options);
 });
+
